caf832594405f7bd65d21abc49bb2ed2e437e2ee
Devin <djaenicke.92@gmail.com>
2020-11-28 11:25:12 -0700

    Created Catkin package and dependency script

---
+++ scripts/depend.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+git clone https://github.com/clearpathrobotics/robot_upstart.git ../../robot_upstart
+git clone https://github.com/Slamtec/rplidar_ros.git ../../rplidar_ros
+git clone https://github.com/ros-drivers/rosserial.git ../../rosserial
